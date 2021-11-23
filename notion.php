05a2939c1fd136be2a2cc3344e6cb517fb9da650
Ruslan Abkadirov <rusabk@yandex.ru>
2021-11-24 01:22:40 +0300

    get_db_info & curls_get function are fixed

--- notion.php
+++ notion.php
@@ -8,16 +8,21 @@ class Notion{
     private $curl;
 
 
-    private function curl_get($url, $headers){
+    private function curl_get($url, $headers = []){
         curl_setopt($this->curl, CURLOPT_POST, false);
 
+        array_push($headers, $this->auth_h, $this->version_h);
         curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($this->curl, CURLOPT_URL, $url);
+
+        return curl_exec($this->curl);
     }
 
     function __construct($key){
         $this->key = $key;
         $this->auth_h = 'Authorization: Bearer ' . $this->key;
+
+        $this->curl = curl_init();
 
         // curl settings ===============
         // return result as a string instead of put it to response
@@ -29,7 +34,7 @@ class Notion{
     }
 
     function get_db_info($id){
-        return curl_get('https://api.notion.com/v1/databases/' . $id);
+        return $this->curl_get('https://api.notion.com/v1/databases/' . $id, []);
     }
 }
 ?>
