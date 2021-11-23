0af58064b4ab0b20e96c3f1bef55841bf3cb7e89
Ruslan Abkadirov <rusabk@yandex.ru>
2021-11-24 01:11:19 +0300

    work started

---
+++ notion.php
@@ -0,0 +1,35 @@
+<?php
+class Notion{
+    private $key;
+    private $auth_h;
+    private $version_h = 'Notion-Version: 2021-08-16';
+
+
+    private $curl;
+
+
+    private function curl_get($url, $headers){
+        curl_setopt($this->curl, CURLOPT_POST, false);
+
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+    }
+
+    function __construct($key){
+        $this->key = $key;
+        $this->auth_h = 'Authorization: Bearer ' . $this->key;
+
+        // curl settings ===============
+        // return result as a string instead of put it to response
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        // save error information to stderr
+        curl_setopt($this->curl, CURLOPT_VERBOSE, 1);
+
+
+    }
+
+    function get_db_info($id){
+        return curl_get('https://api.notion.com/v1/databases/' . $id);
+    }
+}
+?>
